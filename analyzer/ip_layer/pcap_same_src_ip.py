8ad97636de1f8e1050c46e03a50568354821bf6c
jwxie <xiejiawei000@gmail.com>
2021-12-02 14:23:27 +0800

    [add] same ip analysis;

--- analyzer/ip_layer/pcap_same_src_ip.py
+++ analyzer/ip_layer/pcap_same_src_ip.py
@@ -1,18 +1,29 @@
 # coding: utf-8
 # author: jwxie - xiejiawei000@gmail.com
 import socket
-from collections import defaultdict
 
 import dpkt.dns
 
 from package_parser import pcap_parser_generator
 
-RCODE_DICT = {
+RCODE_DICT: dict[int, str] = {
     0: "NOERROR", 1: "FORMERR", 2: "SERVFAIL",
     3: "NXDOMAIN", 4: "NOTIMP", 5: "REFUSED",
     6: "YXDOMAIN", 7: "XRRSET", 8: "NOTAUTH",
     9: "NOTZONE"
 }
+
+
+def _construct_element(_ip, _dns):
+    return {
+        'id': _dns.id,
+        'ip_len': _ip.len,
+        'dns_rcode': _dns.rcode,
+        'dns_rcode_msg': RCODE_DICT[_dns.rcode],
+        'ip_5': (socket.inet_ntoa(_ip.src), _ip.data.sport,
+                 socket.inet_ntoa(_ip.dst), _ip.data.dport,
+                 _ip.len,),
+    }
 
 
 def pcap_same_src_ip(
@@ -25,23 +36,24 @@ def pcap_same_src_ip(
     :return:
     """
 
-    _d = defaultdict(list)
-    for i, (_, ip, _, dns) in enumerate(pcap):
-        src_ip = socket.inet_ntoa(ip.src)
-        src_port = ip.data.get('sport')
-        dst_ip = socket.inet_ntoa(ip.dst)
-        dst_port = ip.data.get('dport')
-
+    ip_pkgs_info = {}
+    for i, (ts, (_, ip, _, dns)) in enumerate(pcap):
         if dns.qr == dpkt.dns.DNS_Q:
-            _d[id].append({
-                'is_query': True,
-                'is_response': False
-            })
+            _d = ip_pkgs_info.get(socket.inet_ntoa(ip.src), {'Q': [], 'R': [], })
+            _d['Q'].append(_construct_element(ip, dns))
+            ip_pkgs_info[socket.inet_ntoa(ip.src)] = _d
+        elif dns.qr == dpkt.dns.DNS_R:
+            _d = ip_pkgs_info.get(socket.inet_ntoa(ip.dst), {'Q': [], 'R': [], })
+            _d['R'].append(_construct_element(ip, dns))
+            ip_pkgs_info[socket.inet_ntoa(ip.dst)] = _d
         else:
             print('报文类型错误，仅支持查询、响应类型的报文')
 
-        for answer in dns.an:
-            a = 10
+    # 后处理一下，统计数据
+    for ip_idx, info in ip_pkgs_info.items():
+        Q, R = info['Q'], info['R']
+
+    return a
 
 
 if __name__ == '__main__':
