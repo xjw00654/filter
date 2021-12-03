efabe46bfefdc1b575fa654c60706bf2054494d9
jwxie <xiejiawei000@gmail.com>
2021-12-03 17:18:30 +0800

    [update] comment;

--- analyzer/dns_layer/functional/whois_query.py
+++ analyzer/dns_layer/functional/whois_query.py
@@ -18,6 +18,12 @@ _RE = re.compile("^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-
 def _is_valid_domain_name(
         domain_name: str
 ) -> bool:
+    """
+    判断是否是正常的域名
+
+    :param domain_name: 需要验证的域名
+    :return:
+    """
     return bool(re.search(_RE, domain_name))
 
 
@@ -28,7 +34,7 @@ def _api_query_batch_async(
         ua: str = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.34'
-) -> dict:
+) -> typing.Union[None, dict]:
     """
     异步任务分发，将给定的域名以batch的形式异步提交到API接口内，返回任务的ID号
 
@@ -59,11 +65,14 @@ def _api_query_batch_async(
             with open(f'whois_batch_query_failed_domain_names_{task_stamp}.txt', 'a+') as fp:
                 for _e in [e for e in dn.split('|') if e]:
                     fp.write(_e + '\n')
-            print(f"第{idx}组任务的{len([e for e in domain_names.split('|') if e])}发放失败，查询失败的域名会记录在"
+            print(f"第{idx}组任务的{len([e for e in dn.split('|') if e])}发放失败，查询失败的域名会记录在"
                   f"{os.path.abspath('./whois_batch_query_failed_domain_names_[当前时间].txt')}文件内")
-    print(f'所有的请求结束，当前的所有任务ID号为:{results}')
 
-    return results
+    print(f'所有的请求结束，当前的所有任务ID号为:{results}')
+    if len(results.keys()):
+        return None
+    else:
+        return results
 
 
 def _api_query(
@@ -73,6 +82,15 @@ def _api_query(
                   'AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.34'
 ) -> typing.Union[dict, None]:
+    """
+    api的提交接口
+
+    :param api_url: 请求的url地址
+    :param domain_name: 域名值
+    :param ua: UA值
+    :return: 正确返回字典，错误返回None
+    """
+
     if not _is_valid_domain_name(domain_name):
         return {'Host': domain_name, 'msg': f'查询失败，给定的域名不符合域名规则（注意不要带上协议头）'}
     else:
@@ -83,10 +101,17 @@ def _api_query(
             return {'Host': domain_name, 'msg': f'查询失败，错误码:{api_response.status_code}'}
 
 
-def whois_api_query_batch_asnyc(
+def whois_api_query_batch_async(
         domain_names: list,
         _n_per_query: int = 5,
-) -> dict:
+) -> typing.Union[None, dict]:
+    """
+    whois批量查询的异步接口
+
+    :param domain_names: 域名列表
+    :param _n_per_query: 每次请求的域名数量，最大不超过50，默认为5
+    :return: 正确返回字典，错误返回None
+    """
     return _api_query_batch_async(
         API_URL.replace('single', 'batch'),
         domain_names,
@@ -100,6 +125,12 @@ def whois_api_query_batch_asnyc(
 def whois_api_query(
         domain_name: str
 ) -> typing.Union[dict, None]:
+    """
+    whois单条域名查询接口
+
+    :param domain_name: 需要查询的域名
+    :return: 正常返回字典，错误返回None
+    """
     return _api_query(
         API_URL,
         domain_name,
@@ -110,4 +141,4 @@ def whois_api_query(
 
 
 if __name__ == '__main__':
-    print(_api_query_batch_async('jwxie.com'))
+    print(whois_api_query_batch_async(['jwxie.com', ]))
