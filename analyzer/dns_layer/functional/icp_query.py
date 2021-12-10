8d50d67bd4120e12a62dab25e1efe00e94cc91da
jwxie <xiejiawei000@gmail.com>
2021-12-10 09:56:05 +0800

    [update] relative import err;

--- analyzer/dns_layer/functional/icp_query.py
+++ analyzer/dns_layer/functional/icp_query.py
@@ -2,7 +2,7 @@
 # author: jwxie - xiejiawei000@gmail.com
 import typing
 
-from whois_query import _api_query
+from .whois_query import _api_query
 
 # 请前往http://my.chinaz.com/ChinazAPI/Statistics/AccessStatistics开通自己的key，免费100条
 ICP_API_KEY = 'e761b1c6f4e14b78ba9d2b9bc1f9e3a4'
