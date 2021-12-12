1aa46febd71db2a89bb035b5c065a664b058843d
jwxie <xiejiawei000@gmail.com>
2021-12-12 10:22:39 +0800

    [update] __init__ file;

--- analyzer/dns_layer/functional/__init__.py
+++ analyzer/dns_layer/functional/__init__.py
@@ -3,11 +3,9 @@
 
 from .d_score import d_score, d_score_fast
 from .domain_name_length import *
-from .domain_name_length import *
 from .domain_name_seperation import domain_name_seperator
 from .entropy import *
-from .gibberish_detect import Client
-from .gibberish_detect.gib_detect import Client as gibberish_query_client
+from .gibberish_detect import Client as gibberish_query_client
 from .icp_query import icp_api_query
 from .ip_bgp_query import Client as ip_bgp_query_client
 from .ip_geoIP import Client as ip_geoip_query_client
