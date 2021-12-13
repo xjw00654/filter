697807ef617b71b6872cb2185c68de5bfda1401a
jwxie <xiejiawei000@gmail.com>
2021-12-13 13:53:16 +0800

    [update] ngram freq features;

--- analyzer/dns_layer/functional/domain_name_nlp_features.py
+++ analyzer/dns_layer/functional/domain_name_nlp_features.py
@@ -2,10 +2,11 @@
 # author: jwxie - xiejiawei000@gmail.com
 import itertools
 import os
+import re
 import typing
+from collections import Counter
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
 def _get_specific_ext_files(path: str, ext: str):
@@ -50,10 +51,9 @@ def get_whitelist_from_txt(
                    [e.strip().split(',')[1] for e in open(txt, 'r', encoding='utf-8').readlines()]
                    if len(elem) >= max_domain_length]
         else:
-            pass
-            # wl += [elem for elem in
-            #        [e.strip().replace('full:', '') for e in open(txt, 'r', encoding='utf-8').readlines()
-            #         if not e.startswith('regexp:')] if len(elem) >= max_domain_length]
+            wl += [elem for elem in
+                   [e.strip().replace('full:', '') for e in open(txt, 'r', encoding='utf-8').readlines()
+                    if not e.startswith('regexp:')] if len(elem) >= max_domain_length]
     return sorted(list(set(wl)))
 
 
@@ -104,18 +104,31 @@ def cal_vowel(
 
 
 def cal_ngram(
+        *,
+        n: int,
         data: list,
-        n: int
+
 ):
+    """
+    计算ngram特征
+
+    :param n: ngram中n
+    :param data: 需要计算的数据
+    :return: 数据的ngram频率
+    """
+    if not isinstance(n, int):
+        raise Exception('参数错误，n需要为一个整数')
     ngram_vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
-    ngram_vectorizer.fit(bl.tolist())
+    whitelist_list_data_rm_ext = [".".join(e.split('.')[:-1]) for e in
+                                  get_whitelist_from_txt(use_v2ray=False, max_domain_length=n + 2)]
+    data_ngram = ngram_vectorizer.fit_transform(whitelist_list_data_rm_ext)
+
+    data_ct = Counter(data_ngram.indices)
     vocab = ngram_vectorizer.vocabulary_
 
-    vocab_sum = sum(vocab.values())
-    ngram_freq_avg = [np.mean([vocab[elem.lower()]
-                               for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in bl]
-    ngram_freq_std = [np.std([vocab[elem.lower()]
-                              for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in bl]
+    ngram_freq = [[data_ct[vocab[elem.lower()]] for elem in re.findall(".{" + str(n) + "}", e)] for e in data]
+
+    return ngram_freq
 
 
 def cal_freq(
@@ -141,92 +154,91 @@ def cal_freq(
     ] for e in data]
     return result
 
-
-if __name__ == '__main__':
-    import re
-    import numpy as np
-    from collections import Counter
-    import pandas as pd
-
-    import matplotlib.pyplot as plt
-    import seaborn
-    from analyzer.dns_layer.functional.entropy import cal_entropy_batch
-    from analyzer.dns_layer.functional.gibberish_detect import Client
-
-    wl = np.array(get_whitelist_from_txt())
-
-    bl = np.array(
-        [elem for elem in
-         [e.strip().split('\t')[:2] for e in open('../../../static_files/dga.txt', 'r', encoding='utf-8').readlines()
-          if not e.startswith('#') and e != "" and e[0] != "\n"] if len(elem[1]) >= 4])
-    cls, bl = bl[:, 0], bl[:, 1]
-
-    label_c = Counter(cls)
-    label_d = {'normal': len(set(cls)), **{e: i for i, e in enumerate(set(cls))}}
-    label_rd = {i: e for i, e in enumerate(set(cls))}
-
-    label = np.array([label_d[e] for e in cls] + [label_d['normal']] * len(wl))
-
-    data = np.append(bl, wl)
-    cls = np.append(cls, np.array(['normal'] * len(wl)))
-    assert len(data) == len(label)
-
-    # gibberish-papers
-    gib_client = Client()
-    bl_gib = [gib_client.query(e)[1] for e in bl]
-    wl_gib = [gib_client.query(e)[1] for e in wl]
-
-    plt.close()
-    seaborn.distplot(bl_gib, color='g')
-    seaborn.distplot(wl_gib, color='orange')
-    plt.savefig(f'../../../static_files/gibberish.png')
-
-    # vowel-papers
-    bl_counter = [len([c for c in e if c.lower() in 'aeiou']) / len(e) for e in bl]
-    wl_counter = [len([c for c in e if c.lower() in 'aeiou']) / len(e) for e in wl]
-
-    plt.close()
-    seaborn.distplot(bl_counter, color='g')
-    seaborn.distplot(wl_counter, color='orange')
-    plt.savefig(f'../../../static_files/vowel_kde.png')
-
-    # ngram-papers
-    for n in range(3, 4):
-        bl_unigram_vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
-        bl_unigram_vectorizer.fit(bl.tolist())
-        bl_vocab = bl_unigram_vectorizer.vocabulary_
-
-        bl_vocab_sum = sum(bl_vocab.values())
-        bl_unigram_freq_avg = [np.mean([bl_vocab[elem.lower()]
-                                        for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in bl]
-        bl_unigram_freq_std = [np.std([bl_vocab[elem.lower()]
-                                       for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in bl]
-
-        wl_unigram_vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
-        wl_unigram_vectorizer.fit(wl.tolist())
-        wl_vocab = wl_unigram_vectorizer.vocabulary_
-
-        wl_vocab_sum = sum(wl_vocab.values())
-        wl_unigram_freq_avg = [np.mean([wl_vocab[elem.lower()]
-                                        for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in wl]
-        wl_unigram_freq_std = [np.std([wl_vocab[elem.lower()]
-                                       for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in wl]
-
-        plt.close()
-        seaborn.distplot(wl_unigram_freq_avg, color='g')
-        seaborn.distplot(bl_unigram_freq_avg, color='orange')
-        plt.savefig(f'../../../static_files/_{n}gram_kde_avg.png')
-
-        plt.close()
-        seaborn.distplot(wl_unigram_freq_std, color='g')
-        seaborn.distplot(bl_unigram_freq_std, color='orange')
-        plt.savefig(f'../../../static_files/_{n}gram_kde_std.png')
-
-    # entropy boxplot
-    data_entropy = cal_entropy_batch(data)
-    df = pd.DataFrame(list(zip(data_entropy, cls)), columns=['entropy', 'label'])
-    plt.figure(figsize=(10, 30))
-    seaborn.boxplot(y='label', x='entropy', data=df, orient="h")
-    plt.savefig('../../../static_files/entropy.png')
-
-    print(get_features(data=['jwxie.cn', ], ngram=(1, 2,), tfidf_feature=True))
+# if __name__ == '__main__':
+#     import re
+#     import numpy as np
+#     from collections import Counter
+#     import pandas as pd
+#
+#     import matplotlib.pyplot as plt
+#     import seaborn
+#     from analyzer.dns_layer.functional.entropy import cal_entropy_batch
+#     from analyzer.dns_layer.functional.gibberish_detect import Client
+#
+#     wl = np.array(get_whitelist_from_txt())
+#
+#     bl = np.array(
+#         [elem for elem in
+#          [e.strip().split('\t')[:2] for e in open('../../../static_files/dga.txt', 'r', encoding='utf-8').readlines()
+#           if not e.startswith('#') and e != "" and e[0] != "\n"] if len(elem[1]) >= 4])
+#     cls, bl = bl[:, 0], bl[:, 1]
+#
+#     label_c = Counter(cls)
+#     label_d = {'normal': len(set(cls)), **{e: i for i, e in enumerate(set(cls))}}
+#     label_rd = {i: e for i, e in enumerate(set(cls))}
+#
+#     label = np.array([label_d[e] for e in cls] + [label_d['normal']] * len(wl))
+#
+#     data = np.append(bl, wl)
+#     cls = np.append(cls, np.array(['normal'] * len(wl)))
+#     assert len(data) == len(label)
+#
+#     # gibberish-papers
+#     gib_client = Client()
+#     bl_gib = [gib_client.query(e)[1] for e in bl]
+#     wl_gib = [gib_client.query(e)[1] for e in wl]
+#
+#     plt.close()
+#     seaborn.distplot(bl_gib, color='g')
+#     seaborn.distplot(wl_gib, color='orange')
+#     plt.savefig(f'../../../static_files/gibberish.png')
+#
+#     # vowel-papers
+#     bl_counter = [len([c for c in e if c.lower() in 'aeiou']) / len(e) for e in bl]
+#     wl_counter = [len([c for c in e if c.lower() in 'aeiou']) / len(e) for e in wl]
+#
+#     plt.close()
+#     seaborn.distplot(bl_counter, color='g')
+#     seaborn.distplot(wl_counter, color='orange')
+#     plt.savefig(f'../../../static_files/vowel_kde.png')
+#
+#     # ngram-papers
+#     for n in range(3, 4):
+#         bl_unigram_vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+#         bl_unigram_vectorizer.fit(bl.tolist())
+#         bl_vocab = bl_unigram_vectorizer.vocabulary_
+#
+#         bl_vocab_sum = sum(bl_vocab.values())
+#         bl_unigram_freq_avg = [np.mean([bl_vocab[elem.lower()]
+#                                         for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in bl]
+#         bl_unigram_freq_std = [np.std([bl_vocab[elem.lower()]
+#                                        for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in bl]
+#
+#         wl_unigram_vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+#         wl_unigram_vectorizer.fit(wl.tolist())
+#         wl_vocab = wl_unigram_vectorizer.vocabulary_
+#
+#         wl_vocab_sum = sum(wl_vocab.values())
+#         wl_unigram_freq_avg = [np.mean([wl_vocab[elem.lower()]
+#                                         for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in wl]
+#         wl_unigram_freq_std = [np.std([wl_vocab[elem.lower()]
+#                                        for elem in re.findall(".{" + str(n) + "}", e)]).tolist() for e in wl]
+#
+#         plt.close()
+#         seaborn.distplot(wl_unigram_freq_avg, color='g')
+#         seaborn.distplot(bl_unigram_freq_avg, color='orange')
+#         plt.savefig(f'../../../static_files/_{n}gram_kde_avg.png')
+#
+#         plt.close()
+#         seaborn.distplot(wl_unigram_freq_std, color='g')
+#         seaborn.distplot(bl_unigram_freq_std, color='orange')
+#         plt.savefig(f'../../../static_files/_{n}gram_kde_std.png')
+#
+#     # entropy boxplot
+#     data_entropy = cal_entropy_batch(data)
+#     df = pd.DataFrame(list(zip(data_entropy, cls)), columns=['entropy', 'label'])
+#     plt.figure(figsize=(10, 30))
+#     seaborn.boxplot(y='label', x='entropy', data=df, orient="h")
+#     plt.savefig('../../../static_files/entropy.png')
+#
+#     print(get_features(data=['jwxie.cn', ], ngram=(1, 2,), tfidf_feature=True))
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 # coding: utf-8
 # author: jwxie - xiejiawei000@gmail.com
-from analyzer.dns_layer.functional import ttl
-from package_parser import pcap_parser_generator
+import itertools
+
+import analyzer.dns_layer.functional as F
+import analyzer.dns_layer.functional.domain_name_nlp_features as nlp
+
+
+def dga_analysis():
+    whitelist_domain_name = nlp.get_whitelist_from_txt(use_v2ray=False)
+    dga_domain_names, dga_domain_types = nlp.get_dga_from_txt()
+    dga_domain_types, dga_domain_names = list(dga_domain_types), list(dga_domain_names)
+
+    data = [e for e in itertools.chain(whitelist_domain_name, dga_domain_names)]  # 去掉suffix
+    label = [0] * len(whitelist_domain_name) + [1] * len(dga_domain_names)
+    sub_label = ['normal'] * len(whitelist_domain_name) + dga_domain_types
+
+    # initialize query client
+    gibberish_client = F.gibberish_query_client()
+
+    data_1gram = nlp.cal_ngram(data=data, n=1)
+    data_entropy = F.cal_entropy_batch(data)
+    data_gibberish = [gibberish_client.query(e) for e in data]
+    data_vowel = nlp.cal_vowel(data)
+    data_freq = nlp.cal_freq(data)
+
+    a = 10
+
 
 if __name__ == '__main__':
-
-    pcap = pcap_parser_generator('D:\\Dataset\\botnet\\botnet2014\\ISCX_Botnet-Testing.dns.pcap')
-
-    for eth, ip, udp, dns in pcap:
-        a = ttl.ttl_filter(dns)
+    dga_analysis()
